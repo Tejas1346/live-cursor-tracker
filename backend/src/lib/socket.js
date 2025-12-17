3d141f5e27a6828009f89a48d2d9794d6e371ba8
Tejas1346 <tejasshyampn@gmail.com>
2025-12-17 14:51:07 +0530

    implemented websockets mvp ready

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,15 +5,25 @@ import { Server } from "socket.io";
 export const app = express();
 app.use(express.json());
 export const server = createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:["http://localhost:5173"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log("connected",socket.id);
+  const userId = socket.id;
+  console.log("connected", userId);
 
-    
+  socket.broadcast.emit("user-joined", {
+    userId: userId,
+  });
+
+  socket.on("mouse-move", ({ x, y }) => {
+    socket.broadcast.emit("cursor-update", {userId, x, y });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-left", { userId: userId });
+  });
 });
-
